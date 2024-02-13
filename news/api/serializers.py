b6274b3b891d2f1fc75c53a4c7ef0c13d3482294
Vadym <petrukvadd@gmail.com>
2024-02-13 20:28:51 +0200

    serializers validation

--- news/api/serializers.py
+++ news/api/serializers.py
@@ -31,3 +31,13 @@ class ArticleSerializer(serializers.Serializer):
         instance.active = validated_data.get('active', instance.active)
         instance.save()
         return instance
+
+    def validate(self, data):
+        """Check that description and title are different"""
+        if data['title'] == data['description']:
+            raise serializers.ValidationError("Title and description must be different")
+        return data
+    
+    def validate_title(self, value):
+        if len(value) < 60:
+            raise serializers.ValidationError("The title is too small")
